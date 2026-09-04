50ae7e398f8699523b7be1544a8a8435d0c4288b
Daniel Mayo <itiswellwithinmysoul@gmail.com>
2026-09-04 02:32:28 -0500

    feat(shared): add main index file

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Shared types and constants for sweepstakes casino
+ */
+
+// Game Types
+export * from "./types/game";
+export * from "./types/api";
+export * from "./types/payout";
+export * from "./types/admin";
+
+// Constants
+export * from "./constants/game";
+
+// Re-exports for convenience
+export { SymbolType } from "./types/game";
+export { AdminPermission } from "./types/admin";
